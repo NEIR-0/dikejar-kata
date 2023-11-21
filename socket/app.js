d3dd62af16d71c0247766a852040677211f621ad
AsmodayCelestia <herusyeme@gmail.com>
2023-11-21 23:12:31 +0700

    olla

--- socket/app.js
+++ socket/app.js
@@ -1,17 +1,46 @@
+
 const express= require('express')
 const socketIo = require("socket.io");
 const http = require("http");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express()
 const server = http.createServer(app);
+const axios = require('axios')
 
 const io = socketIo(server, {
   cors: {
     origin: 'http://localhost:5173',
   },
 }); //in case server and client run on different urls
+
+// module.exports = io
+
+
 io.on("connection", (socket) => {
   console.log("client connected: ", socket.id);
+
+  socket.on('CLIENT_JOIN', async(params) => {
+    const {gameId, access_token} = params
+    console.log(gameId, access_token);
+
+    // check ke db apakah sudah pernah join sebelumnya
+    try {
+    const {data}= await axios.get("http://localhost:3000/games/" + gameId, {
+        headers: {
+            authorization: "Bearer " + access_token
+        }
+    })
+
+    socket.join(gameId);
+    io.to(gameId).emit('SERVER_JOINED',data);
+
+
+    } catch (error) {
+        // room penuh
+        console.log(error);
+    }
+
+    })
 
   socket.on("disconnect", (reason) => {
     console.log(reason);
