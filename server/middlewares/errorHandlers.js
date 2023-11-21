8963f56eb57af648b6ef8019f5d150350b3fc857
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2023-11-21 19:33:57 +0700

    fix: changed GET /games/:gameId

--- server/middlewares/errorHandlers.js
+++ server/middlewares/errorHandlers.js
@@ -2,6 +2,10 @@ function errorHandlers(error, req, res, next) {
   switch (error.name) {
     case "unauthorized":
       return res.status(401).json({ message: error.message });
+    case "forbidden":
+      return res.status(403).json({ message: error.message });
+    case "notFound":
+      return res.status(404).json({ message: error.message });
     default:
       console.log(error);
       return res.status(500).json({ message: "Internal Server Error" });
