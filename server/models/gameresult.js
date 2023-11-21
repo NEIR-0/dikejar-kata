970e21afa89a37c16aac7f74a54860123983ac18
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2023-11-21 14:50:59 +0700

    setup: db models & migrations

---
+++ server/models/gameresult.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class GameResult extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  GameResult.init({
+    GameId: DataTypes.INTEGER,
+    WinnerId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'GameResult',
+  });
+  return GameResult;
+};
