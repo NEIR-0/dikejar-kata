0558838a2d3c08f1b85f01989fc88358afc3ad95
AsmodayCelestia <herusyeme@gmail.com>
2023-11-22 13:01:36 +0700

    home-styling

--- client/src/views/home.jsx
+++ client/src/views/home.jsx
@@ -34,30 +34,57 @@ function RoomList() {
 
   return (
     <>
-      <section className="w-full h-fit relative">
-        <LogoutBtn logout={logout} />
-        <div className="p-5 flex justify-center">
-          {/* search */}
-          <form className="flex absolute top-[13%]">
-            <input className="px-5 py-2 w-[400px] rounded-s-md" type="text" />
-            <button className="px-3 py-2 bg-slate-600 text-white">
-              <i className="fa-solid fa-magnifying-glass fa-rotate-90"></i>
-            </button>
-          </form>
+      <section className="bgImg w-full h-screen flex-cols justify-center items-center">
+          <section>
+            <LogoutBtn logout={logout} />
+          </section>
 
-          {/* create room */}
-          <Link to="/createRoom" className="absolute top-[13%] right-20 px-3 py-2 bg-white">
-            <i className="fa-solid fa-plus"></i>
-          </Link>
+          <section className="mx-12">
+            <div className="relative w-1/3">
+                <form >
+                    <input
 
+                    type="search"
+                    id="search-dropdown"
+                    className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                    placeholder="Search Mockups, Logos, Design Templates..."
+                    required=""
+                    />
+                    <button
+                    type="submit"
+                    className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                    <svg
+                        className="w-4 h-4"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 20 20"
+                    >
+                        <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                        />
+                    </svg>
+                    <span className="sr-only">Search</span>
+                    </button>
+                </form>
+            </div>
+          </section>
           {/* list */}
-          <div className="mt-[10%] w-full h-fit p-2 flex flex-wrap justify-center">
-            {room &&
-              room.map((el) => {
-                return <CardRoom key={el.id} data={el} />;
-              })}
-          </div>
-        </div>
+          <section>
+            <main>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-12 w-full h-fit p-2 flex-wrap justify-center">
+                {room &&
+                  room.map((el) => {
+                    return <CardRoom key={el.id} data={el} />;
+                  })}
+              </div>
+            </main>
+          </section>
       </section>
     </>
   );
